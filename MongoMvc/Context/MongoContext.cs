556696c579407ffc1e7cd7349653852ba2f7c939
Frank Odoom <f.odoom@outlook.com>
2018-04-11 10:57:28 +0000

    Added MongDb custom event handlers for tracing database server events and states

--- MongoMvc/Context/MongoContext.cs
+++ MongoMvc/Context/MongoContext.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver;
+﻿using log4net;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
 using MongoMvc.POCOs;
 using MongoMvc.Properties;
 using System;
@@ -14,10 +16,27 @@ namespace MongoMvc.Context
 
         public MongoContext()
         {
-            
-            var client = new MongoClient(Settings.Default.MongoConn);         
+
+            //var client = new MongoClient(Settings.Default.MongoConn);         
+            //Database = client.GetDatabase(Settings.Default.Database);
+
+            //Create a settings object and add event subscribtions
+            var conString = Settings.Default.MongoConn;
+            var settings = MongoClientSettings.FromUrl(new MongoUrl(conString));
+            var client = new MongoClient(settings);
+
+            //setup clustor configurator to listen to events
+            //settings.ClusterConfigurator = builder => builder.Subscribe<CommandStartedEvent>(started=>{
+
+            //});
+
+            settings.ClusterConfigurator = builder => builder.Subscribe<Log4netEvents>(started => {
+
+            });
+
+
             Database = client.GetDatabase(Settings.Default.Database);
-      
+
         }
 
         public IMongoCollection<Rental> Rentals => Database.GetCollection<Rental>("rentals");
@@ -26,5 +45,73 @@ namespace MongoMvc.Context
         {
            
         }
+    }
+
+
+    //Wiring up custom Event Handler
+    internal class Log4netEvents : IEventSubscriber
+    {
+        public static ILog CommandStartedLog = LogManager.GetLogger("CommandStarted");
+        private ReflectionEventSubscriber _Subscriber;
+        public Log4netEvents()
+        {
+            _Subscriber = new ReflectionEventSubscriber(this);
+        }
+
+        //event tracing using log4net
+        public bool TryGetEventHandler<TEvent>(out Action<TEvent> handler)
+        {
+            return _Subscriber.TryGetEventHandler(out handler);
+        }
+
+        public void Handle(CommandStartedEvent started)
+        {
+            CommandStartedLog.Info(new
+            {
+                started.Command,
+                started.CommandName,
+                started.ConnectionId,
+                started.DatabaseNamespace,
+                started.OperationId,
+                started.RequestId
+                
+            
+            });
+        }
+
+        public void Handle(ConnectionClosedEvent closed)
+        {
+            CommandStartedLog.Info(new
+            {
+                closed.ClusterId,
+                closed.ConnectionId,
+                closed.Duration,
+                closed.ServerId
+
+            });
+        }
+
+        public void Handle(CommandSucceededEvent succeed)
+        {
+
+        }
+
+
+        //optional event tracing
+        //public bool TryGetEventHandler<TEvent>(out Action<TEvent> handler)
+        //{
+        //    if(typeof (TEvent) != typeof(CommandStartedEvent))
+        //    {
+        //        handler = null;
+        //        return false;
+        //    }
+        //    handler = e =>
+        //     {
+        //         //you can return back an event handler of your choice
+        //     };
+        //    return true;
+        //}
+
+
     }
 }
---
+++ MongoMvc/Controllers/DynamicFormController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.SqlServer.Server;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoMvc.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace MongoMvc.Controllers
+{
+    public class DynamicFormController : Controller
+    {
+        // GET: DynamicForm
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PostDynamic(FormCollection data)
+        {
+            using (var context = new MongoContext())
+            {
+                var json = FormSerializer(data);
+                var collection = context.Database.GetCollection<BsonDocument>("questionnaire");
+                var document = BsonSerializer.Deserialize<BsonDocument>(json);
+                collection.InsertOne(document);
+            }
+            
+             return View("index");
+        }
+
+        public string FormSerializer(FormCollection collection)
+        {
+            var list = new Dictionary<string, string>();
+            foreach (string key in collection.Keys)
+            {
+             
+                list.Add(key, collection[key]);
+            }
+            list.Remove("__RequestVerificationToken");
+            return new JavaScriptSerializer().Serialize(list);
+        }
+
+    }
+}
